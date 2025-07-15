d444fe48472f1fe6a639197ff00de553e2e7e0ea
khahnm04 <khanhnguyenminh1811@gmail.com>
2025-07-15 11:18:40 +0700

    init project

--- src/test/java/com/projects/laptopshop/LaptopShopApplicationTests.java
+++ src/test/java/com/projects/laptopshop/LaptopShopApplicationTests.java
@@ -1,10 +1,10 @@
-package vn.hoidanit.laptopshop;
+package com.projects.laptopshop;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LaptopshopApplicationTests {
+class LaptopShopApplicationTests {
 
 	@Test
 	void contextLoads() {
