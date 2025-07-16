9c849fdf7f1dd05b8239542cdac79eb848aad19a
khahnm04 <khanhnguyenminh1811@gmail.com>
2025-07-16 20:53:22 +0700

    #28. Spring Beans

--- src/main/java/com/projects/laptopshop/LaptopShopApplication.java
+++ src/main/java/com/projects/laptopshop/LaptopShopApplication.java
@@ -2,12 +2,16 @@ package com.projects.laptopshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class LaptopShopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LaptopShopApplication.class, args);
+		ApplicationContext beans = SpringApplication.run(LaptopShopApplication.class, args);
+		for(String s: beans.getBeanDefinitionNames()) {
+			System.out.println(s);
+		}
 	}
 
 }
