41588c26570035f83b77bfb35fe36807fd4a00df
khahnm04 <khanhnguyenminh1811@gmail.com>
2025-07-21 12:10:30 +0700

    #52. Chữa Bài Tập Giao diện Tạo Mới (CREATE) User

--- src/main/java/com/projects/laptopshop/controller/UserController.java
+++ src/main/java/com/projects/laptopshop/controller/UserController.java
@@ -3,23 +3,33 @@ package com.projects.laptopshop.controller;
 import com.projects.laptopshop.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     private UserService userService;
 
-    public UserController(UserService userService) {
+    public UserController(
+        UserService userService
+    ) {
         this.userService = userService;
     }
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String getHomePage(Model model) {
         String test = this.userService.handleHello();
         model.addAttribute("value1", test);
         model.addAttribute("value2", "from controller with model");
         return "hello";
+    }
+
+    @GetMapping("/admin/user")
+    public String getUserPage(Model model) {
+        String test = this.userService.handleHello();
+        model.addAttribute("test", test);
+        model.addAttribute("value", "from controller with model");
+        return "admin/user/create";
     }
 
 }
