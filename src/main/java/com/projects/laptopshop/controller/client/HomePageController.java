a5eb60d0b59e0effbe3a81e5662cc59744a792ea
khahnm04 <khanhnguyenminh1811@gmail.com>
2025-08-10 10:22:26 +0700

    #108. DTO - Data Transfer Object

--- src/main/java/com/projects/laptopshop/controller/client/HomePageController.java
+++ src/main/java/com/projects/laptopshop/controller/client/HomePageController.java
@@ -1,10 +1,13 @@
 package com.projects.laptopshop.controller.client;
 
 import com.projects.laptopshop.domain.Product;
+import com.projects.laptopshop.domain.dto.RegisterDTO;
 import com.projects.laptopshop.service.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -26,6 +29,12 @@ public class HomePageController {
 
     @GetMapping("/register")
     public String getRegisterPage(Model model) {
+        model.addAttribute("registerUser", new RegisterDTO());
+        return "client/auth/register";
+    }
+
+    @PostMapping("/register")
+    public String handleRegister(@ModelAttribute("registerUser") RegisterDTO registerDTO) {
 
         return "client/auth/register";
     }
