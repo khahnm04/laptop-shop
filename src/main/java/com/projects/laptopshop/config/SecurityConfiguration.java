4558bde81470d711007afa5cd91b2b1dd324891c
khahnm04 <khanhnguyenminh1811@gmail.com>
2025-08-11 15:19:11 +0700

    #117. Spring Security Custom Login Page

--- src/main/java/com/projects/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/com/projects/laptopshop/config/SecurityConfiguration.java
@@ -2,16 +2,16 @@ package com.projects.laptopshop.config;
 
 import com.projects.laptopshop.service.CustomUserDetailsService;
 import com.projects.laptopshop.service.UserService;
+import jakarta.servlet.DispatcherType;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
@@ -39,18 +39,18 @@ public class SecurityConfiguration {
         return authProvider;
     }
 
-//    @Bean
-//    public AuthenticationManager authenticationManager(
-//        HttpSecurity http,
-//        PasswordEncoder passwordEncoder,
-//        UserDetailsService userDetailsService
-//    ) throws Exception {
-//        AuthenticationManagerBuilder authenticationManagerBuilder = http
-//                .getSharedObject(AuthenticationManagerBuilder.class);
-//        authenticationManagerBuilder
-//                .userDetailsService(userDetailsService)
-//                .passwordEncoder(passwordEncoder);
-//        return authenticationManagerBuilder.build();
-//    }
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorize -> authorize
+                    .dispatcherTypeMatchers(DispatcherType.FORWARD, DispatcherType.INCLUDE).permitAll()
+                    .requestMatchers("/", "/login", "/client/**", "/admin/css/**", "/admin/js/**", "/admin/images/**").permitAll()
+                    .anyRequest().authenticated())
+            .formLogin(formLogin -> formLogin
+                    .loginPage("/login")
+                    .failureUrl("/login?error")
+                    .permitAll());
+        return http.build();
+    }
 
 }
