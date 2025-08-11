2374c8741c03a222bfc5a39b768bbd53185b933e
khahnm04 <khanhnguyenminh1811@gmail.com>
2025-08-11 10:54:06 +0700

    #116. Debug Spring Security (Part 2)

--- src/main/java/com/projects/laptopshop/config/SecurityConfiguration.java
+++ src/main/java/com/projects/laptopshop/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import com.projects.laptopshop.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -27,17 +28,29 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(
-        HttpSecurity http,
+    public DaoAuthenticationProvider authProvider(
         PasswordEncoder passwordEncoder,
         UserDetailsService userDetailsService
-    ) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder = http
-                .getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder);
-        return authenticationManagerBuilder.build();
+    ) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder);
+        authProvider.setHideUserNotFoundExceptions(false);
+        return authProvider;
     }
+
+//    @Bean
+//    public AuthenticationManager authenticationManager(
+//        HttpSecurity http,
+//        PasswordEncoder passwordEncoder,
+//        UserDetailsService userDetailsService
+//    ) throws Exception {
+//        AuthenticationManagerBuilder authenticationManagerBuilder = http
+//                .getSharedObject(AuthenticationManagerBuilder.class);
+//        authenticationManagerBuilder
+//                .userDetailsService(userDetailsService)
+//                .passwordEncoder(passwordEncoder);
+//        return authenticationManagerBuilder.build();
+//    }
 
 }
